c61ab2d2afde396098ed21dcdd0775fd49a427a5
Földi Tamás <tfoldi@xsi.hu>
2021-03-20 11:02:09 +0100

    dynamic table and column name

--- bodo-tabpy-ipyparallel/lr-bodo-tabpy.py
+++ bodo-tabpy-ipyparallel/lr-bodo-tabpy.py
@@ -11,13 +11,12 @@ import tabpy
 from tabpy.tabpy_tools.client import Client
 
 
-
 @bodo.jit
-def lr_from_sql(conn):
-    df = pd.read_sql('select * from lr_points limit 100', conn)
+def lr_from_sql(conn, sql, response_col):
+    df = pd.read_sql(sql, conn)
 
-    X = df.drop(columns=["response"]).to_numpy()
-    Y = df["response"].to_numpy()
+    X = df.drop(columns=[response_col]).to_numpy()
+    Y = df[response_col].to_numpy()
     D = X.shape[1]
     w = np.ones(D) - 0.5
     for i in range(10):
@@ -25,15 +24,17 @@ def lr_from_sql(conn):
     return w
 
 
-def lr_from_sql_wrapper():
-    return lr_from_sql(os.environ['SNOWFLAKE_URL']).tolist()
+def lr_from_sql_wrapper(args):
+    table, response_col = args
+    sql = f"select * from {table} limit 100"
+    return lr_from_sql(os.environ['SNOWFLAKE_URL'], sql, response_col).tolist()
 
 
-def lr_snowflake(*args):
+def lr_snowflake(args=["lr_points","response"]):
     client = ipp.Client(profile="default")
     dview = client[:]
 
-    ar = dview.apply(lr_from_sql_wrapper, *args)
+    ar = dview.apply(lr_from_sql_wrapper, args)
     res = ar.get()
     return res[0]
 
@@ -49,8 +50,9 @@ def main():
 
     tp_client = Client('http://localhost:8080/')
     tp_client.deploy('lr_snowflake',
-                  lr_snowflake,
-                  'Logistic regression from Snowflake table', override=True)
+                     lr_snowflake,
+                     'Logistic regression from Snowflake table', override=True)
+
 
 if __name__ == "__main__":
     main()
